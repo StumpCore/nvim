00bc8836b1f58180248de1700423255a4e6627e0
StumpCore <marc.schinschel@googlemail.com>
2024-09-24 18:57:56 +0200

    Adding Rust support

--- init.lua
+++ init.lua
@@ -111,6 +111,17 @@ require('lazy').setup({
   'roxma/vim-hug-neovim-rpc',
 }, {})
 
+local rt = require("rust-tools")
+rt.setup({
+  server = {
+    on_attach = function(_,bufnr)
+      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, {buffer = bufnr})
+      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+    end,
+  }
+})
+
+
 -- [[ Setting options ]]
 vim.g.nvim_tree_hide_dotfiles = 1
 vim.g.nvim_tree_indent_markers = 1 
